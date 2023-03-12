f7a4cb458a657e709231e6d3458ef0910ec98e0d
minhduc184 <nmhwww3@gmail.com>
2023-03-12 19:51:14 +0700

    update

--- server.js
+++ server.js
@@ -1,17 +1,28 @@
-// See https://github.com/typicode/json-server#module
 const jsonServer = require('json-server')
+const express = require('express');
+const path = require('path');
+
 const server = jsonServer.create()
 const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
 
 server.use(middlewares)
+server.use('/public', express.static(path.join(__dirname, 'public')))
 // Add this before server.use(router)
 server.use(jsonServer.rewriter({
     '/api/*': '/$1',
     '/blog/:resource/:id/show': '/:resource/:id'
 }))
+
+// Serve homepage with links to all APIs
+server.get('/', (req, res) => {
+    const resources = Object.keys(router.db.__wrapped__)
+    const links = resources.map(resource => `<li><a href="/${resource}">${resource}</a></li>`)
+    res.send(`<h1>APIs:</h1><ul>${links.join('')}</ul>`)
+})
+
 server.use(router)
-server.listen(3000, () => {
+server.listen(process.env.PORT || 3000, () => {
     console.log('JSON Server is running')
 })
 
